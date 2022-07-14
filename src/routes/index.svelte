b2b5b6e62e69e75dae0420bee4db331ee6b65f0f
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-07-14 00:52:01 -0400

    Added favicon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,3 @@
 <script lang="ts">
-    import pubEnv from '$lib/env/pubEnv';
-    import { session } from '$app/stores';
-    import trpcClient from '../lib/trpcClient';
 
-    let greeting = '<null>';
-
-    async function rpcTest() {
-        greeting = await trpcClient.query('hello');
-    }
 </script>
-
-<h1>{greeting}</h1>
-<h3>{pubEnv.test}</h3>
-<button on:click={rpcTest}>rpc test</button>
-<a href="/api/auth/signin/discord?redirect=https://localhost:3000/">login</a>
-
-<hr />
-
-{#if $session.user}
-    {JSON.stringify($session.user)}
-    <hr />
-    <img src={$session.user.pfp} alt='test'>
-{/if}
-
-<hr />
-
-{#if $session.profile}
-    {JSON.stringify($session.profile)}
-{/if}
